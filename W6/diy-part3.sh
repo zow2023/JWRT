4a4ae5fd8c9e2f0ec6f78bddb553381bd520b0d8
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-04-26 23:10:57 +0800

    Update diy-part3.sh

--- W6/diy-part3.sh
+++ W6/diy-part3.sh
@@ -33,7 +33,7 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # Modify default IP
-sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
