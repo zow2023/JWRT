b2fa918ffbcd60abdacd86a9daadd9fae9c48d68
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-05-01 00:06:57 +0800

    Update and rename diy-part1.sh to diy-part1.sh

--- E8450/diy-part1.sh
+++ E8450/diy-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone https://github.com/lgs2007m/luci-app-easytier package/easytier
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
