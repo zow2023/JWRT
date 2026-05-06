5c3e349e087f9a4db78cdd7921c5244487ecb0e9
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-05-06 16:12:48 +0800

    Update diy-part2.sh

--- W6/diy-part2.sh
+++ W6/diy-part2.sh
@@ -11,15 +11,15 @@
 #
 
 # 生成 MT7986 (FIRST_IF) 的头文件
-python3 -c '
-import os
-with open("package/mtk/drivers/mt_wifi/files/mt7986-predator-w6/MT7986_ePAeLNA_EEPROM_AX7800.bin", "rb") as f:
+#python3 -c '
+#import os
+#with open("package/mtk/drivers/mt_wifi/files/mt7986-predator-w6/MT7986_ePAeLNA_EEPROM_AX7800.bin", "rb") as f:
     data = f.read()
-with open("package/mtk/drivers/mt_wifi/src/mt_wifi/embedded/include/eeprom/mt7986_e2p_ePAeLNA.h", "w") as out:
-    out.write("static const UCHAR MT7986_E2PImage_ePAeLNA[] = {\n")
-    out.write(", ".join([f"0x{b:02x}" for b in data]))
-    out.write("\n};\n")
-'
+#with open("package/mtk/drivers/mt_wifi/src/mt_wifi/embedded/include/eeprom/mt7986_e2p_ePAeLNA.h", "w") as out:
+#    out.write("static const UCHAR MT7986_E2PImage_ePAeLNA[] = {\n")
+ #   out.write(", ".join([f"0x{b:02x}" for b in data]))
+ #   out.write("\n};\n")
+#'
 
 #git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
 #git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
@@ -32,11 +32,11 @@ with open("package/mtk/drivers/mt_wifi/src/mt_wifi/embedded/include/eeprom/mt798
 
 ##git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
 
-git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
-git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+#git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
+#git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+#git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+#git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+#git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 
 # 移除 openwrt feeds 自带的核心包
