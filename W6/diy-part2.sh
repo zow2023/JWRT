b6c8f90027ea14ff75cbdd65dbcad6018b8b646a
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-05-14 17:32:03 +0800

    Update diy-part2.sh

--- W6/diy-part2.sh
+++ W6/diy-part2.sh
@@ -49,9 +49,9 @@ rm -rf feeds/luci/applications/luci-app-daed
 rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/packages/net/{dae,daed}
 
-git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
 
-git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-timecontrol
+#git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-timecontrol
 #git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
 #git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
@@ -60,8 +60,8 @@ git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-time
 #git clone https://github.com/zow2023/luci-app-dae package/dae
 #git clone https://github.com/QiuSimons/luci-app-dae -b kix package/dae
 
-git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 rm -rf package/helloworld/luci-app-dae
 rm -rf package/helloworld/luci-app-daed
 rm -rf package/helloworld/dae
