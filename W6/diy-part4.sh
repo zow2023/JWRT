5eeaef50b76f5ea81dc511f79ddf077a823b6a95
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-05-17 19:38:56 +0800

    Uncomment git clone commands and remove comments

--- W6/diy-part4.sh
+++ W6/diy-part4.sh
@@ -10,11 +10,11 @@
 # See /LICENSE for more information.
 #
 
-git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
-git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+#git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
+#git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+#git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+#git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+#git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 rm -rf feeds/packages/net/adguardhome
@@ -23,13 +23,13 @@ rm -rf feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-timecontrol
 
-#git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
-#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
+git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 rm -rf package/helloworld/dae
 
-#rm -rf feeds/packages/lang/node
+rm -rf feeds/packages/lang/node
 #git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
-#git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
