2a90709a277269953d3c60eae298ef0742dcb4bc
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-06-08 00:23:42 +0800

    Update diy-part2.sh

--- EA7500/diy-part2.sh
+++ EA7500/diy-part2.sh
@@ -16,7 +16,7 @@
 # git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
-
+rm -rf feeds/luci/applications/luci-app-passwall
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
