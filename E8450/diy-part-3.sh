dc80843a0061ba77d3c3ff7b18ddf8a2308ab0d1
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-06-15 12:47:17 +0800

    Create diy-part-3.sh

---
+++ E8450/diy-part-3.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+
+# 移除 openwrt feeds 自带的核心包
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/luci/applications/luci-app-adguardhome
+git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
+git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+
+rm -rf package/helloworld/luci-app-dae
+rm -rf package/helloworld/luci-app-daed
+rm -rf package/helloworld/dae
+rm -rf feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node -b packages-25.12 feeds/packages/lang/node
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+# Modify default IP
+sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/E8450/g' package/base-files/files/bin/config_generate
