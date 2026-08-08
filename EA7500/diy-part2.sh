8e1b9d99c65a05da6ca5cad98e9533a117ba673d
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-08-08 22:53:56 +0800

    Update diy-part2.sh

--- EA7500/diy-part2.sh
+++ EA7500/diy-part2.sh
@@ -11,7 +11,7 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
 
 #git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
 
@@ -29,10 +29,10 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # Modify default IP
-sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # OpenWrt 24.10 精确修改 mac80211.uc 默认开启无线
-sed -i 's/\${defaults ? 0 : 1}/0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+#sed -i 's/\${defaults ? 0 : 1}/0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
