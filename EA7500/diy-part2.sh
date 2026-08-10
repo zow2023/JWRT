69ce61fa9a8598c622eb8cbbec7791c18dbb3bd5
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-08-10 21:01:33 +0800

    Update diy-part2.sh

--- EA7500/diy-part2.sh
+++ EA7500/diy-part2.sh
@@ -5,11 +5,11 @@
 # git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 
-git clone --depth https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
 #git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone --depth https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+git clone  https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
 #git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-git clone --depth https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+git clone  https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
@@ -20,15 +20,15 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 #git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-timecontrol
 #git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
-git clone --depth https://github.com/zow2023/openwrt_helloworld package/helloworld
+git clone  https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 rm -rf feeds/packages/lang/node
-git clone --depth https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
+git clone  https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
-git clone --depth https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
