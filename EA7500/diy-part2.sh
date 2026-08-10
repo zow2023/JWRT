447e4580d5da6c65bdc432ed8e43da6fff4d49ce
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-08-10 12:20:59 +0800

    Update diy-part2.sh

--- EA7500/diy-part2.sh
+++ EA7500/diy-part2.sh
@@ -11,7 +11,7 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-#rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
 
 #git clone https://github.com/OneNAS-space/luci-app-adguardhome package/luci-app-adguardhome
 
