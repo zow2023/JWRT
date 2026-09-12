5223b3867b0304ce76ee8ef02f9f0b24dc3d9168
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-09-12 12:14:22 +0800

    Update diy-part5.sh

--- W6/diy-part5.sh
+++ W6/diy-part5.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
@@ -7,22 +9,31 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/zow2023/luci-app-adguardhome.git package/luci-app-adguardhome
+rm -rf package/luci-app-adguardhome/patches
 
 git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
+git clone https://github.com/zow2023/luci-app-honk package/honk
 git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+rm -rf package/helloworld/dae
 
 rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node -b packages-25.12 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 27.x feeds/packages/lang/golang
 
 # Modify default IP
 sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# OpenWrt 24.10 精确修改 mac80211.uc 默认开启无线
+sed -i 's/\${defaults ? 0 : 1}/0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 sed -i 's/OpenWrt/W6-WRT/g' package/base-files/files/bin/config_generate
+
+echo "CONFIG_DEVEL=y" >> .config
+echo "CONFIG_CCACHE=y" >> .config
