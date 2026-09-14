a0faa3794f954f50fc1d98e711f9ef191a76769e
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-09-14 08:24:19 +0800

    Update diy-part-4.sh

--- E8450/diy-part-4.sh
+++ E8450/diy-part-4.sh
@@ -5,6 +5,7 @@ rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-adguardhome
 #git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/zow2023/luci-app-adguardhome package/luci-app-adguardhome
+rm -rf package/luci-app-adguardhome/patches
 #git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
 rm -rf feeds/luci/applications/luci-app-dae
