a55c0e1a18c4804770b9c7bcec28d3db752b0e37
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-09-16 11:02:11 +0800

    Update diy-part-3.sh

--- E8450/diy-part-3.sh
+++ E8450/diy-part-3.sh
@@ -13,18 +13,19 @@ rm -rf feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/zow2023/luci-app-adguardhome.git package/luci-app-adguardhome
 rm -rf package/luci-app-adguardhome/patches
 git clone https://github.com/VizzleTF/luci-theme-footstrap package/luci-theme-footstrap
-git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+#git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 
 git clone https://github.com/zow2023/luci-app-meow package/meow
 git clone https://github.com/zow2023/InfinityDuck package/new/InfinityDuck
 git clone https://github.com/zow2023/luci-app-honk package/honk
 
-git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/kenzok8/small package/small
 
-#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 rm -rf package/helloworld/luci-app-dae
 rm -rf package/helloworld/luci-app-daed
-rm -rf package/helloworld/dae
+rm -rf package/helloworld/mihomo-alpha
+rm -rf package/helloword/luci-app-ssr-plus
 
 rm -rf feeds/packages/lang/node
 #git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
